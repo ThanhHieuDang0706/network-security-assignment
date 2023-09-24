27324273eb6e14e285381281bd74cb9e1e64138a
Đặng Thanh Hiếu <hieu.dang.dth_is_me@hcmut.edu.vn>
2023-09-24 18:58:09 +0700

    inital commit

--- code/rail-fence/rail_fence_cypher.py
+++ code/rail-fence/rail_fence_cypher.py
@@ -1,10 +1,10 @@
 from base_cypher import BaseCipher
 
 class RailFenceCypher(BaseCipher):
-    def encrypt(self, plain_text: str, key: str) -> str:
+    def encrypt(plain_text: str, key: str) -> str:
         pass
 
-    def decrypt(self, plain_text: str, key: str) -> str:
+    def decrypt(plain_text: str, key: str) -> str:
         pass
 
     def try_decrypt_without_key(plain_text: str) -> str:
