5a1e86f58e260ed72476631871bcaf8cdd4bb53a
MrHighlandCN <trancaonguyen0901@gmail.com>
2023-11-30 20:47:03 +0700

    Update Ceasar and Perm cipher

--- backend/app/core/ciphers/cesar/cesar_cipher.py
+++ backend/app/core/ciphers/cesar/cesar_cipher.py
@@ -1,18 +1,61 @@
+
 from core.ciphers.base_cipher import BaseCipher
+from core.ciphers.constants import CipherType, NOT_FOUND_KEY
 import core.ciphers.constants as constants
+import string
+
 
 class CesarCipher(BaseCipher):
+    alphabet = tuple(string.ascii_uppercase)
+    SIZE = len(alphabet)
+
     def __init__(self):
         self.type = constants.CipherType.CESAR
 
     def encrypt(self, plain_text: str, key: str) -> str:
-        pass
+        # create sub alphabet
+        subAlphabet = []
+        for i in range(self.SIZE):
+            if i + key >= self.SIZE:
+                subAlphabet.append(self.alphabet[key - (self.SIZE - i)])
+            else:
+                subAlphabet.append(self.alphabet[i + key])
 
-    def decrypt(self, cipher_text: str, key: str) -> str:
-        pass
+        # create a dict store key = character in alphabet corresponding to value = character in sub alphabet
+        arr = {}
+        for i in range(self.SIZE):
+            arr[f"{self.alphabet[i]}"] = f"{subAlphabet[i]}"
+        # string store cipher text
+        str = ""
+        # start to encrypt
+        for i in range(len(plain_text)):
+            if plain_text[i].isupper():
+                str += arr[f"{plain_text[i]}"]
+            elif plain_text[i].islower():
+                str += arr[f"{plain_text[i].upper()}"].lower()
+            else:
+                str += plain_text[i]
+
+        # return value
+        return str
+
+    def decrypt(self, cipher_text: str, key: int) -> str:
+        return self.encrypt(cipher_text, self.SIZE - key)
 
     def try_decrypt_without_key(self, cipher_text: str) -> dict[int, str]:
-        pass
+        result = {}
+        for i in range (1, self.SIZE):
+            result[i] = self.decrypt(cipher_text, i)
+        return result
 
     def try_get_key(self, plain_text: str, cipher_text: str) -> str:
-        pass
+        if len(cipher_text) != len(plain_text):
+            return NOT_FOUND_KEY
+        
+        for i in range(1, self.SIZE):
+            if plain_text == self.decrypt(cipher_text, i):
+                return i
+        
+        return KEY_NOT_FOUND
+
+
--- backend/app/core/ciphers/cesar_rail_fence_perm/cesar_rail_fence_perm_cipher.py
+++ backend/app/core/ciphers/cesar_rail_fence_perm/cesar_rail_fence_perm_cipher.py
@@ -1,17 +1,37 @@
 from core.ciphers.base_cipher import BaseCipher
-from core.ciphers.constants import CipherType
+from core.ciphers.constants import CipherType, NOT_FOUND_KEY
+from core.ciphers.cesar.cesar_cipher import CesarCipher
+from core.ciphers.rail_fence.rail_fence_cipher import RailFenceCipher
 class CesarRailFencePermCipher(BaseCipher):
     def __init__(self):
         self.type = CipherType.PERM_CESAR_RAIL_FENCE
 
-    def encrypt(self, plain_text: str, key: int) -> str:
-        pass
+    def encrypt(self, plain_text: str, ckey: int, rkey: int) -> str:
+        ceasarText = CesarCipher()
+        railfenceText = RailFenceCipher()
+        ceasarRes = ceasarText.encrypt(plain_text, ckey)
+        return railfenceText.encrypt(ceasarRes, rkey)
 
-    def decrypt(self, cipher_text: str, key: int) -> str:
-        pass
+    def decrypt(self, cipher_text: str, ckey: int, rkey) -> str:
+        ceasarText = CesarCipher()
+        railfenceText = RailFenceCipher()
+        raifenceRes = railfenceText.decrypt(cipher_text, rkey)
+        return ceasarText.decrypt(raifenceRes, ckey)
 
     def try_decrypt_without_key(self, cipher_text: str) -> dict[int, str]:
-        pass
+        ceasarText = CesarCipher()
+        result = {}
+        for i in range (2, len(cipher_text)):
+            for j in range(1, ceasarText.SIZE):
+                result[(i, j)] = self.decrypt(cipher_text, j, i)
+        return result
 
-    def try_get_key(plain_text: str, cipher_text: str) -> int:
-        pass
+    def try_get_key(self, plain_text: str, cipher_text: str) -> int:
+        if len(plain_text) != len(cipher_text):
+            return KEY_NOT_FOUND
+        
+        for i in range (2, len(cipher_text)):
+            for j in range(1, ceasarText.SIZE):
+                if plain_text == self.decrypt(cipher_text, j, i):
+                    return (i, j)
+        return KEY_NOT_FOUND
