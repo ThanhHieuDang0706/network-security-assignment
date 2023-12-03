37833779271ef5537ce005598ef3ad804a8d0277
Đặng Thanh Hiếu <hieu.dang.dth_is_me@hcmut.edu.vn>
2023-12-03 15:34:22 +0700

    remove deploy on vercel and use render

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,8 +26,3 @@ app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, 
 @app.get("/", tags="Root")
 async def read_root():
     return {"message": "Welcome to this fantastic app!"}
-
-if __name__ == "__main__":
-    import uvicorn
-    port=int(getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port, reload=False)
