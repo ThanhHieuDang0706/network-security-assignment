6efdac2cfb4c22bcde4d712e05f904e209d2980d
Đặng Thanh Hiếu <hieu.dang.dth_is_me@hcmut.edu.vn>
2023-12-03 13:09:17 +0700

    try deploy fastapi app

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from .routers import ciphers
 from .constants import APP_TITLE
 from .core.english_dict import ENGLISH_DICTIONARY
-import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 
 """ write code to test here """
@@ -22,8 +21,4 @@ async def load_dictionary():
             global ENGLISH_DICTIONARY
             ENGLISH_DICTIONARY[line.strip()] = True
 
-
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
